82629281eb663ae6d12fdbc159e358183a6f4c2e
Abdoul Sadikou <sadikouhak@gmail.com>
2022-02-16 21:09:22 -0500

    update

---
+++ project1/tp1/scrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# URL de l'index des tests (trouvée en regardant la source de n'importe quel test...)
+URL=https://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/html/full/flat/index.html
+
+
+# Validation du nom du dossier pour conenir les pages html
+if [[ -z $1 ]]; then
+  echo "Inscrivez le nom du dossier apres $(basename $0)" #ici monrep
+  exit 1
+fi
+
+# Creation du dossier
+mkdir -p $1
+
+# Scrapping
+wget --recursive -l 1 --no-host-directories --directory-prefix=$1 --no-parent robots=off -nd --reject "index.html*" --reject "robots.txt*" $URL
