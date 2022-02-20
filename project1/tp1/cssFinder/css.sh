23eee1460f29cb1c15d505452a73b64c5aa3dd7c
MargueriteMireille <66399926+MargueriteMireille@users.noreply.github.com>
2022-02-19 23:14:37 -0500

    done for real

--- project1/tp1/cssFinder/css.sh
+++ project1/tp1/cssFinder/css.sh
@@ -7,10 +7,13 @@
 #  	Marguerite Camara 20143122   #
 ##################################
 
-i=1
+#i=1
 for FILE in "../monrep"/*; do
-    python3 beautifulsoup.py $FILE $i
-    sass --error-css "mySaas$i.scss" "style$i.css"
+
+	i1=${FILE//[!0-9]/}
+    i2=${i1#*3}
+    python3 beautifulsoup.py $FILE $i2
+    sass --error-css "mySaas$i2.scss" "style$i2.css"
     #echo $saasFile
     #print(css)
     i=$((i+1))
