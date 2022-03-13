7715d72beed2a610723c5022947fb04b00417464
MargueriteMireille <66399926+MargueriteMireille@users.noreply.github.com>
2022-03-12 23:59:00 -0500

    Rien

--- project2/question2.js
+++ project2/question2.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
         $("#interfaceConsultation").hide();
         $("#concept").hide();
         $("#interfaceJeux").show();
+        $("#recherche").hide();
       
     });
 
