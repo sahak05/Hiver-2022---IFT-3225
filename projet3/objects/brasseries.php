7cf67d7e51e89f36c4fdceb2f32eddacf0f2af0e
MargueriteMireille <66399926+MargueriteMireille@users.noreply.github.com>
2022-04-15 17:29:53 -0400

    delete

--- projet3/objects/brasseries.php
+++ projet3/objects/brasseries.php
@@ -197,6 +197,31 @@ class Brasseries{
 	}
 
 
+	function delete(){
+	
+		$query = "DELETE FROM " . $this->table_name . " WHERE nom = ?";
+
+
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+
+		// sanitize
+		$this->id=htmlspecialchars(strip_tags($this->id));
+
+		// bind id of record to delete
+		$stmt->bindParam(1, $this->id);
+
+		// execute query
+		if($stmt->execute()){
+
+			return true;
+		}
+
+		return false;
+
+	}
+
+
 
 
 }
